c66d4845aaee90f933a1268da102f0c240184f53
Nick Sexton <nick.sexton@gmail.com>
2014-11-08 19:12:52 +0000

    ex 2.4.6

---
+++ exercise_2.4.5_metropolis_normal.m
@@ -0,0 +1,79 @@
+# Use the Metropolis procedure to sample from Cauchy density
+1; # not a function file
+function y = cauchy(theta)
+  # returns the unnormalized density of the cauchy distribution
+  y = 1 ./ (1 + theta .^ 2);
+end
+
+function y = norm_proportional (theta)
+  y = exp(-1 * theta .^ 2 + 2 .* theta);
+end
+
+function y = norm_withparams (theta, sigma, mu)
+  y = 1/sigma * exp((-1 * theta .^ 2) + (2 * mu .* theta) - (mu .^ 2) );
+end
+
+
+
+# Initialise the Metropolis sampler
+T = 10000; # max iterations
+burnin = 500; # samples to exclude
+sigma = 0.5 # std dev of normal proposal density
+thetamin  = -30; thetamax = 30; # define a range for starting values
+
+target_mu = -5# mean of target (normal);
+target_sigma = 20 # sd of target (normal);
+
+theta = zeros(1, T); # init storage space for our samples
+# seed=1; rand('state', seed); randn('state',seed); # set the random seed
+theta(1) = unifrnd (thetamin, thetamax);
+accepted = [0 0]; # accept : reject
+
+
+
+# start sampling
+t = 1;
+while t < T # iterate until we have T samples
+  t=t+1;
+  # propose a new value for theta using a normal proposal density
+  theta_star = normrnd (theta(t-1), sigma);
+  # calculate the acceptance ratio
+  alpha = min ([ 1 (norm_withparams( theta_star, target_sigma, ...
+				    target_mu) / 
+		    norm_withparams(theta(t-1), target_sigma, target_mu ))]);
+  # draw a uniform deviate from [0. 1]
+  u = rand;
+  # do we accept this proposal?
+  if u < alpha
+     theta(t) = theta_star; # if so, proposal becomes a new state
+     accepted(1) ++;
+  else
+    theta(t) = theta(t-1); # if not, copy old state
+    accepted(2) ++;
+  end
+end
+
+# Display histogram of our samples
+figure (1); clf;
+subplot (3,1,1);
+nbins = 200;
+thetabins = linspace (thetamin, thetamax, nbins);
+counts = hist (theta(burnin:T), thetabins);
+bar (thetabins, counts/sum(counts), 'k');
+xlim ([thetamin thetamax]);
+xlabel ('\theta'); ylabel ('p(\theta)');
+
+# overlay the theoretical density
+y = norm_withparams (thetabins, target_sigma, target_mu); # use normpdf instead?
+hold on;
+plot (thetabins, y/sum(y), 'r--', 'LineWidth', 3);
+set (gca, 'YTick', []);
+
+# display history of our samples
+subplot (3,1,2:3);
+stairs (theta, 1:T, 'k-');
+ylabel ('t'); xlabel ('\theta');
+set (gca, 'YDir', 'reverse');
+xlim ([thetamin thetamax]);
+
+accepted(1) / sum(accepted)
---
+++ exercise_2.4.6_4chains.m
@@ -0,0 +1,63 @@
+# Use the Metropolis procedure to sample from Cauchy density
+1; # not a function file
+function y = cauchy(theta)
+  # returns the unnormalized density of the cauchy distribution
+  y = 1 ./ (1 + theta .^ 2);
+end
+
+
+# Initialise the Metropolis sampler
+T = 1000; # max iterations
+burnin = 500; # samples to exclude
+sigma = 1.5 # std dev of normal proposal density
+thetamin  = -30; thetamax = 30; # define a range for starting values
+theta = zeros(4, T); # init storage space for our samples
+# seed=1; rand('state', seed); randn('state',seed); # set the random seed
+theta(1,:) = unifrnd (thetamin, thetamax);
+
+
+
+# start sampling
+t = 1;
+while t < T # iterate until we have T samples
+  t=t+1;
+  # propose a new value for theta using a normal proposal density
+  theta_star = normrnd (theta(t-1,:), sigma);
+  # calculate the acceptance ratio
+  alpha = min ([ 1 (cauchy( theta_star(1,:)) / cauchy (theta(t-1,:) ))]);
+  # draw a uniform deviate from [0. 1]
+  u = rand;
+  # do we accept this proposal?
+  for (i = 1:columns(theta))
+    if u < alpha
+      theta(t,i) = theta_star(1,i); # if so, proposal becomes a new state
+    else
+      theta(t,i) = theta(t-1,i); # if not, copy old state
+    endif
+  end
+end
+
+# Display histogram of our samples
+figure (1); clf;
+subplot (3,1,1);
+nbins = 200;
+thetabins = linspace (thetamin, thetamax, nbins);
+counts = hist (theta(burnin:T), thetabins);
+bar (thetabins, counts/sum(counts), 'k');
+xlim ([thetamin thetamax]);
+xlabel ('\theta'); ylabel ('p(\theta)');
+
+# overlay the theoretical density
+y = cauchy (thetabins);
+hold on;
+plot (thetabins, y/sum(y), 'r--', 'LineWidth', 3);
+set (gca, 'YTick', []);
+
+# display history of our samples
+subplot (3,1,2:3);
+stairs (theta, 1:T, 'k-');
+ylabel ('t'); xlabel ('\theta');
+set (gca, 'YDir', 'reverse');
+xlim ([thetamin thetamax]);
+
+accepted(1) / sum(accepted)
